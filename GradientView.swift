00e7b42eee887ef9330edd77fa43aeb97351a036
LorenzOliveto <lorenzo.oliveto@mumbleideas.it>
2018-03-09 10:54:52 +0100

    First Commit

---
+++ GradientView.swift
@@ -0,0 +1,121 @@
+//
+//  GradientView.swift
+//  GradientView
+//
+//  Created by Lorenzo Oliveto on 09/03/18.
+//  Copyright © 2018 Mumble. All rights reserved.
+//
+
+import UIKit
+
+class GradientView: UIView {
+    private var gradientLayer: CAGradientLayer!
+    
+    @IBInspectable var colors: [Any]? {
+        didSet {
+            self.gradientLayer.colors = colors
+        }
+    }
+    
+    /* The start and end points of the gradient when drawn into the layer's
+     * coordinate space. The start point corresponds to the first gradient
+     * stop, the end point to the last gradient stop. Both points are
+     * defined in a unit coordinate space that is then mapped to the
+     * layer's bounds rectangle when drawn. (I.e. [0,0] is the bottom-left
+     * corner of the layer, [1,1] is the top-right corner.) The default values
+     * are [.5,0] and [.5,1] respectively. Both are animatable. */
+    var startPoint: CGPoint? {
+        didSet {
+            if let startPoint = startPoint {
+                self.gradientLayer.startPoint = startPoint
+            }
+            else {
+                self.gradientLayer.startPoint = CGPoint(x: 0.5, y:0)
+            }
+        }
+    }
+    
+    var endPoint: CGPoint? {
+        didSet {
+            if let endPoint = endPoint {
+                self.gradientLayer.endPoint = endPoint
+            }
+            else {
+                self.gradientLayer.endPoint = CGPoint(x: 0.5, y:1)
+            }
+        }
+    }
+
+    // MARK: - Initializers
+    
+    /**
+     Initializes the view with a frame and an array of colors for the gradient.
+     
+     - Parameter frame:   The frame for the view.
+     - Parameter colors: The colors of the gradient.
+     
+     */
+    convenience init(frame: CGRect, colors: [Any]){
+        self.init(frame: frame, colors: colors, startPoint: nil, endPoint: nil)
+    }
+    
+    /**
+     Initializes the view with a frame, an array of colors for the gradient and the start and end point of the gradient.
+     
+     - Parameter frame:   The frame for the view.
+     - Parameter colors: The colors of the gradient.
+     - Parameter startPoint: The startPoint of the gradient.
+     - Parameter endPoint: The endPoint of the gradient.
+     
+     */
+    convenience init(frame: CGRect, colors: [Any], startPoint: CGPoint?, endPoint: CGPoint?){
+        self.init(frame: frame)
+        self.colors = colors
+        self.startPoint = startPoint
+        if let endPoint = startPoint {
+            self.gradientLayer.endPoint = endPoint
+        }
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        self.initGradientLayer()
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.initGradientLayer()
+    }
+    
+    private func initGradientLayer(){
+        self.gradientLayer = CAGradientLayer()
+        self.gradientLayer.frame = self.bounds
+        
+        if let colors = self.colors {
+            self.gradientLayer.colors = colors
+        }
+        
+        self.layer.masksToBounds = true
+        self.layer.insertSublayer(self.gradientLayer, at: 0)
+    }
+    
+    // MARK: - Layout
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        if let gradientLayer = self.gradientLayer {
+            gradientLayer.frame = self.bounds
+        }
+    }
+    
+    
+    /**
+     Set the start and end point of the gradient.
+     - Parameter startPoint:   start point.
+     - Parameter endPoint:   end point.
+     */
+    func set(startPoint: CGPoint, endPoint: CGPoint){
+        self.gradientLayer.startPoint = startPoint
+        self.gradientLayer.endPoint = endPoint
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,38 @@
+# GradientView
+
+A view with a configurable gradient.
+
+## Installation
+
+Add to your Podfile
+
+```
+pod 'GradientView', git: 'https://github.com/lorenzOliveto/GradientView.git'
+```
+
+Then run
+```
+pod install
+```
+## Usage
+
+Drag a view in your storyboard and change its class to` GradientView`.
+Then configure it in your ViewController.
+
+Example:
+
+```
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var gradientView: GradientView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.gradientView.colors = [UIColor.yellow.cgColor, UIColor.red.cgColor]
+        self.gradientView.set(startPoint: CGPoint(x: 0.5, y: 0.5), endPoint: CGPoint(x: 1, y: 0.5))
+    }
+}
+```
